0982c69d04e586ee1b80c4463086ef2ab1fbffde
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-02 22:47:24 -0300

    ✨ feat: integra `react-hook-form` ao formulário de cadastro

--- src/pages/Anuncie/index.tsx
+++ src/pages/Anuncie/index.tsx
@@ -1,3 +1,4 @@
+import { useForm } from 'react-hook-form'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/store'
 import Header from '@/components/Header'
@@ -9,29 +10,38 @@ const Anuncie = () => {
         state.categorias.map(({ nome, id }) => ({ nome, id }))
     )
 
+    const { register, handleSubmit } = useForm()
+
+    const cadastrar = (parametro) => {
+        console.log(parametro)
+    }
+
     return (
         <div className={styles.container}>
             <Header
                 titulo="Anuncie aqui!"
                 descricao="Anuncie seu produto no melhor site do Brasil!"
             />
-            <form className={styles.formulario}>
+            <form className={styles.formulario} onSubmit={handleSubmit(cadastrar)}>
                 <input
                     type="text"
                     placeholder="Nome do produto"
                     alt="nome do produto"
+                    {...register('nome')}
                 />
                 <input
                     type="text"
                     placeholder="Descrição do produto"
                     alt="descrição do produto"
+                    {...register('descricao')}
                 />
                 <input
                     type="text"
                     placeholder="URL da imagem do produto"
                     alt="URL da imagem do produto"
+                    {...register('imagem')}
                 />
-                <select defaultValue="default">
+                <select defaultValue="default" {...register('categoria')}>
                     <option value="default" disabled>
                         Selecione a categoria
                     </option>
@@ -41,7 +51,11 @@ const Anuncie = () => {
                         </option>
                     ))}
                 </select>
-                <input type="number" placeholder="Preço do produto" />
+                <input
+                    type="number"
+                    placeholder="Preço do produto"
+                    {...register('preco')}
+                />
                 <Button type="submit">Cadastrar Produto</Button>
             </form>
         </div>
