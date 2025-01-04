21f61d873614be9be083dedb27c2d38155edbf11
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-04 18:11:28 -0300

    ♻️ refactor: atualiza campos da interface `DadosCadastroProduto`

--- src/types/DadosCadastroProduto.ts
+++ src/types/DadosCadastroProduto.ts
@@ -1,7 +1,7 @@
 export interface DadosCadastroProduto {
-    nome: string
+    titulo: string
     descricao: string
-    imagem: string
+    foto: string
     categoria: string
     preco: number
 }
