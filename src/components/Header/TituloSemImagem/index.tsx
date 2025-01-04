6109450b507ab22af1354bdac93b173c631bc96a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-04 20:12:52 -0300

    ✨ feat: adiciona suporte a `children` no componente `TituloSemImagem`

--- src/components/Header/TituloSemImagem/index.tsx
+++ src/components/Header/TituloSemImagem/index.tsx
@@ -1,15 +1,21 @@
 import styles from './TituloSemImagem.module.scss'
 
 interface TituloSemImagemProps {
+    children?: React.ReactNode
     titulo: string
     descricao: string
 }
 
-const TituloSemImagem = ({ titulo, descricao }: TituloSemImagemProps) => {
+const TituloSemImagem = ({
+    children,
+    titulo,
+    descricao,
+}: TituloSemImagemProps) => {
     return (
         <div className={styles.container}>
             <h1 className={styles.titulo}>{titulo}</h1>
             <h2 className={styles.descricao}>{descricao}</h2>
+            {children}
         </div>
     )
 }
