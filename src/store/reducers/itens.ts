719b6f5f7114c8c672a9f677ec051c704f287406
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-06 21:46:23 -0300

    ✨ feat: adiciona lógica da action `deletarItem` para remover corretamente um item do estado

--- src/store/reducers/itens.ts
+++ src/store/reducers/itens.ts
@@ -253,7 +253,9 @@ const itensSlice = createSlice({
             Object.assign(state[index], payload.item)
         },
         deletarItem: (state, { payload }) => {
-            console.log(payload)
+            const index = state.findIndex(item => item.id === payload)
+
+            state.splice(index, 1)
         }
     },
 })
